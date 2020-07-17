fc8e819161385d56afa3b95c81dc2e73a4e8dae2
unknown <rnd@jaewoncnc.co.kr>
2020-07-17 18:23:17 +0900

    init1

---
+++ readdir3.js
@@ -0,0 +1,4 @@
+var fs = require('fs');
+fs.readFile('C:\\project\\PHP_WEB\\public\\dist\\js\\validation.js', 'utf8', function(err, data){
+    console.log(data);
+});
---
+++ readdir4.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var readline = require('readline');
+
+// 한 줄씩 읽어들이는 함수 정의
+function processFile(filename) {
+    var instream = fs.createReadStream(filename);
+    var reader = readline.createInterface(instream, process.stdout);
+
+    var count = 0;
+
+    // 한 줄씩 읽어들인 후에 발생하는 이벤트
+    reader.on('line', function(line) {
+        console.log('한 줄 읽음 : ' + line);
+        count += 1;
+
+        // 공백으로 구분
+        var tokens = line.split(' ');
+        console.log(tokens[2]);
+        if (tokens != undefined && tokens.length > 0) {
+            console.log('#' + count + ' -> ' + tokens[0]);
+        }
+    });
+
+    // 모두 읽어들였을 때 발생하는 이벤트
+    reader.on('close', function(line) {
+        console.log('파일을 모두 읽음.');
+    });
+}
+
+// 함수 실행
+var filename = 'C:\\project\\PHP_WEB\\public\\dist\\js\\validation.js';
+processFile(filename);
