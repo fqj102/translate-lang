4ee1e17a5b30c63db4af1138a59988d1d387049c
unknown <rnd@jaewoncnc.co.kr>
2020-07-22 11:57:38 +0900

    z_app04

--- z_app04.js
+++ z_app04.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const readline = require('readline');
 const translate = require('@vitalets/google-translate-api');
-function fn_convert(text, from, to){
+
+function fn_convert(text, from, to) {
     translate(text, {from, to}).then(res => {
         console.log(res.text);
     }).catch(err => {
@@ -9,16 +10,18 @@ function fn_convert(text, from, to){
     });
 }
 
-
 const sleep = (ms) => {
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
     })
 }
-const lang = {ko:'ko',zh:'zh-CN',en:'en',de:'de'};
+
+
+const lang = {ko: 'ko', zh: 'zh-CN', en: 'en', de: 'de'};
 const lineArr = [];
 const fromLang = lang.ko;
 const toLang = lang.de;
+
 // read by line
 async function readLine(filename) {
     var instream = fs.createReadStream(filename);
@@ -26,31 +29,27 @@ async function readLine(filename) {
     var count = 0;
 
     reader.on('line', function (line) {
-        count+=1;
-        const obj = {line:count,ko:line}
+        count += 1;
+        const obj = {line: count, ko: line}
         lineArr.push(obj);
     });
 
-    reader.on('close',  async (line) => {
-        // await sleep(1000);
-        // fn_convert(i,li,'ko','zh-CN');
+    reader.on('close', async (line) => {
         let message = '';
-        for(let i=0;i<lineArr.length;i++) {
+        for (let i = 0; i < lineArr.length; i++) {
             const li = lineArr[i];
-            message+=('\n'+li.ko);
+            message += ('\n' + li.ko);
             //limit 100 line
-            if(i%100 == 99) {
+            if (i % 100 == 99) {
                 await sleep(2000);
-                fn_convert(message,fromLang,toLang);
+                fn_convert(message, fromLang, toLang);
                 message = '';
             }
         }
         await sleep(2000);
-        if(message) {
-            fn_convert(message,fromLang,toLang);
+        if (message) {
+            fn_convert(message, fromLang, toLang);
         }
-        //lineArr.map(li=>console.log(li));
-
     });
 }
 
